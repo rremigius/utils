1c82904ba7e455fc1656dd41ffd98268663a9151
Remi <remialkemade@gmail.com>
2017-01-05 12:30:37 +0100

    Using Babelify

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 (function(){
     var isNode = typeof module !== 'undefined' && typeof module.exports !== 'undefined';
 
-    var Utils = require('./utils-core').Utils;
+    var Utils = require('./src/utils-core').Utils;
 
     var loaded = [];
-    loaded.push(require('./utils/string').String);
-    loaded.push(require('./utils/validation').Validation);
-    loaded.push(require('./utils/error').Error);
+    loaded.push(require('./src/utils/string').String);
+    loaded.push(require('./src/utils/validation').Validation);
+    loaded.push(require('./src/utils/error').Error);
 
     for(var i=0; i<loaded.length; i++) {
         Utils.extendUtils(loaded[i]);
