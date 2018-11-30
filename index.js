20d2db0a058fb5a4cd00273da922f375ca044029
remi <remi@remialkemade.nl>
2018-11-30 15:38:29 +0100

    Execution utils now use Promises (with backward compatibility for Deferred).

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const _ = require('lodash');
 const utils = {
 	String: require('./src/string'),
 	Validation: require('./src/validation'),
-	Error: require('./src/error'),
 	DOM: require('./src/dom'),
 	Execution: require('./src/execution')
 };
+const Error = require('./src/error');
 
 const Utils = require('./src/extend-utils');
 Utils.extendUtils(_, ['toString']);
@@ -14,5 +14,6 @@ Utils.extendUtils(_, ['toString']);
 for(let key in utils) {
 	Utils.extendUtils(utils[key]);
 }
+Utils.Error = Error;
 
 module.exports = Utils;
--- src/execution.js
+++ src/execution.js
@@ -1,219 +1,236 @@
-const $ = require('jquery');
 const _ = require('lodash');
-const { validate, def } = require('./validation');
+const {validate, def} = require('./validation');
 const Err = require('./error');
 
 const Execution = {};
 
-Execution.Deferred = $.Deferred;
+class DeferredPromise extends Promise {
+	constructor(...args) {
+		super(...args);
+	}
+
+	done(...args) {
+		return this.then(...args);
+	}
+
+	fail(...args) {
+		return this.catch(...args);
+	}
+}
+
 
 /**
  * Tests whether the object is a Promise object (with done and fail methods).
  * @param value
  * @returns {boolean}
  */
-Execution.isPromise = function(value) {
-  if(!_.isObject(value)) {
-    return false;
-  }
-  if (typeof value.then !== "function") {
-    return false;
-  }
-  if (typeof value.catch !== "function") {
-    return false;
-  }
+Execution.isPromise = function (value) {
+	if (!_.isObject(value)) {
+		return false;
+	}
+	if (typeof value.then !== "function") {
+		return false;
+	}
+	if (typeof value.catch !== "function") {
+		return false;
+	}
 
-  return true;
+	return true;
+};
+Execution.isDeferred = function (value) {
+	if (!_.isObject(value)) {
+		return false;
+	}
+	if (typeof value.done !== "function") {
+		return false;
+	}
+	if (typeof value.fail !== "function") {
+		return false;
+	}
+
+	return true;
 };
 
 /**
  * Handles direct values and promises in the same way. If the given value is a promise, it is handled async.
  * Otherwise, the promise is resolved immediately.
- * @param value				 Value or promise.
+ * @param value                    Value or promise.
  * @param {function} [failWhen] A function to check whether a value is a considered a failure value.
- *							  Should return TRUE if value should cause promise to be rejected.
- * @return {$.Deferred}	A promise for the result.
+ *                                Should return TRUE if value should cause promise to be rejected.
+ * @return {DeferredPromise}    A promise for the result. Can also act as a Deferred object.
  */
-Execution.promise = function(value, failWhen) {
-  // To check if resulting value should resolve or reject promise
-  if(!_.isFunction(failWhen)) {
-    failWhen = function(val) { return false; }; // by default, never reject values
-  }
+Execution.promise = function (value, failWhen) {
+	if (Execution.isPromise(value)) {
+		return value;
+	}
 
-  var deferred = new Execution.Deferred();
-  if(Execution.isPromise(value)) {
-    value.then(function(result) {
-      if(failWhen(result)) {
-        deferred.reject(result);
-      } else {
-        deferred.resolve(result);
-      }
-    });
-    value.catch(function(result) {
-      deferred.reject(result);
-    });
-    return value;
-  }
+	// To check if resulting value should resolve or reject promise
+	if (!_.isFunction(failWhen)) {
+		failWhen = function (val) {
+			return false;
+		}; // by default, never reject values
+	}
 
-  // If failWhen is specified, check value
-  if(failWhen(value)) {
-    deferred.reject(value);
-  } else {
-    deferred.resolve(value);
-  }
-  return deferred.promise();
+	return new DeferredPromise((resolve, reject) => {
+		if (Execution.isDeferred(value)) {
+			value.done(resolve);
+			value.fail(reject);
+		}
+		// If failWhen is specified, check value
+		if (failWhen(value)) {
+			reject(value);
+		} else {
+			resolve(value);
+		}
+	});
 };
 
 /**
  * Waits for all deferred and resolves with an object of their results.
- * @param {object} deferredMap   Object of Deferred objects.
- * @param {int} [timeout]		Maximum time to wait before throwing a timeout error.
- * @returns {$.Deferred}	Will contain either a map of results, or an Error object.
+ * @param {object} promises   Object of Deferred objects.
+ * @param {int} [timeout]        Maximum time to wait before throwing a timeout error.
+ * @returns {DeferredPromise}    Will contain either a map of results, or an Error object.
  */
-Execution.waitForAll = function(deferredMap, timeout) {
-  var deferred = new Execution.Deferred();
-  var check = validate({
-    deferredMap  : [deferredMap, 'isObject'],
-    timeout	  : [timeout, 'isNumber', {default: 60000, warn: def(timeout)}]
-  });
-  if(!check.isValid()) {
-    deferred.reject(new Err({message: "Could not wait for deferred. Invalid arguments.", data: {}, errorMap: {}}));
-    return deferred.promise();
-  }
-  var valid = check.getValue();
+Execution.waitForAll = function (promises, timeout) {
+	return new DeferredPromise((resolve, reject) => {
+		// Validation
+		const check = validate({
+			promises: [promises, 'isObject'],
+			timeout: [timeout, 'isNumber', {default: 60000, warn: def(timeout)}]
+		});
+		if (!check.isValid()) {
+			return reject(new Err({
+				message: "Could not wait for deferred. Invalid arguments.",
+				data: {},
+				errorMap: {}
+			}));
+		}
+		const valid = check.getValue();
 
-  if(_.isArray(valid.deferredMap)) {
-    var newObj = {};
-    valid.deferredMap.forEach(function(item, i) {
-      newObj[i] = item;
-    });
-    valid.deferredMap = newObj;
-  }
-  if(Object.keys(valid.deferredMap).length === 0) {
-    deferred.resolve({});
-    return deferred.promise();
-  }
+		if (_.isArray(valid.promises)) {
+			var newObj = {};
+			valid.promises.forEach(function (item, i) {
+				newObj[i] = item;
+			});
+			valid.promises = newObj;
+		}
+		if (Object.keys(valid.promises).length === 0) {
+			return resolve({});
+		}
 
-  var timedOut = false;
-  var state = {};
-  var results = {};
-  var errors = {};
+		// Set Timeout
+		let timedOut = false;
+		let state = {};
+		let results = {};
+		let errors = {};
+		let _timeout = setTimeout(function () {
+			timedOut = true;
+			for (let i in state) {
+				if (state[i].status === 'pending') {
+					errors[i] = new Err("Timed out.");
+				}
+			}
+			reject(new Err({
+				code: 'timeout',
+				message: "Timeout during async operations.",
+				data: results,
+				errorMap: errors
+			}));
+		}, valid.timeout);
 
-  var _timeout = setTimeout(function() {
-    timedOut = true;
-    for(var i in state) {
-      if(state[i].status === 'pending') {
-        errors[i] = new Err("Timed out.");
-      }
-    }
-    deferred.reject(new Err({
-      code: 'timeout',
-      message: "Timeout during async operations.",
-      data: results,
-      errorMap: errors
-    }));
-  }, valid.timeout);
+		const __checkState = function () {
+			let done = true;
+			for (let i in state) {
+				if (state[i].status === 'pending') {
+					done = false;
+					break;
+				}
+			}
+			if (done) {
+				clearTimeout(_timeout);
+				if (Object.keys(errors).length > 0) {
+					reject(new Err({
+						message: "Error(s) occurred during async operations.",
+						errorMap: errors,
+						data: results
+					}));
+				} else {
+					resolve(results)
+				}
+			}
+		};
 
-  var __checkState = function() {
-    var done = true;
-    for(var i in state) {
-      if(state[i].status === 'pending') {
-        done = false;
-        break;
-      }
-    }
-    if(done) {
-      clearTimeout(_timeout);
-      if(Object.keys(errors).length > 0) {
-        deferred.reject(new Err({message: "Error(s) occurred during async operations.", errorMap: errors, data: results}));
-      } else {
-        deferred.resolve(results)
-      }
-    }
-  };
-
-  Object.keys(valid.deferredMap).forEach(function(i) { // this is necessary for callbacks with i
-    if(!Execution.isPromise(valid.deferredMap[i])) {
-      state[i] = {
-        status: 'rejected',
-        data: new Err("Not a promise.")
-      };
-      errors[i] = state[i].data
-    }
-    state[i] = {
-      status: 'pending',
-      data: undefined
-    };
-    valid.deferredMap[i].then(function(result) {
-      if(!timedOut) {
-        state[i].status = 'resolved';
-        state[i].data = result;
-        results[i] = state[i].data;
-        __checkState();
-      }
-    });
-    valid.deferredMap[i].catch(function(result) {
-      if(!timedOut) {
-        state[i].status = 'rejected';
-        state[i].data = result;
-        errors[i] = state[i].data;
-        __checkState();
-      }
-    });
-  });
-
-  return deferred.promise();
+		// Handle promises
+		Object.keys(valid.promises).forEach(function (i) { // this is necessary for callbacks with i
+			let promise = Execution.promise(valid.promises[i]);
+			state[i] = {
+				status: 'pending',
+				data: undefined
+			};
+			promise.then(function (result) {
+				if (!timedOut) {
+					state[i].status = 'resolved';
+					state[i].data = result;
+					results[i] = state[i].data;
+					__checkState();
+				}
+			});
+			promise.catch(function (result) {
+				if (!timedOut) {
+					state[i].status = 'rejected';
+					state[i].data = result;
+					errors[i] = state[i].data;
+					__checkState();
+				}
+			});
+		});
+	});
 };
 
 /**
- * Same as waitForAll, but returns a Promise instead of a Deferred.
+ * Alias for waitForAll
  * @param promises
  * @param timeout
  */
-Execution.awaitAll = function(promises, timeout) {
-  return new Promise((resolve, reject) => {
-    Execution.waitForAll(promises, timeout).then(resolve).catch(reject);
-  });
+Execution.awaitAll = function (promises, timeout) {
+	return Execution.waitForAll(promises, timeout);
 };
 
-Execution.synchronize = function(steps) {
-  var deferred = new Execution.Deferred();
+Execution.synchronize = function (steps) {
+	return new DeferredPromise((resolve, reject) => {
+		const next = function (i, previousResult) {
+			if (i > keys.length - 1) {
+				resolve();
+				return;
+			}
 
-  if(!_.isObject(steps)) {
-    deferred.reject(new Err("Steps must be an object or array."));
-    return deferred.promise();
-  }
+			let key = keys[i];
+			let step = steps[key];
+			if (!_.isFunction(step)) {
+				reject(new Err("Step '" + key + "' is not a function."));
+				return;
+			}
 
-  var keys = Object.keys(steps);
-  if(keys.length === 0) {
-    return deferred.resolve().promise();
-  }
+			Execution.promise(step(previousResult), function (val) {
+				return val instanceof Err;
+			}).then(function (result) {
+				next(i + 1, result)
+			}).catch(function (err) {
+				reject(err);
+			});
+		};
 
-  var next = function(i, previousResult) {
-    if(i > keys.length -1) {
-      deferred.resolve();
-      return;
-    }
+		if (!_.isObject(steps)) {
+			return reject(new Err("Steps must be an object or array."));
+		}
 
-    var key = keys[i];
-    var step = steps[key];
-    if(!_.isFunction(step)) {
-      deferred.reject(new Err("Step '" + key + "' is not a function."));
-      return;
-    }
+		let keys = Object.keys(steps);
+		if (keys.length === 0) {
+			return resolve()
+		}
 
-    Execution.promise(step(previousResult), function(val) { return val instanceof Err; })
-      .then(function(result) {
-        next(i+1, result)
-      })
-      .catch(function(err) {
-        deferred.reject(err);
-      });
-  };
-
-  next(0);
-
-  return deferred.promise();
+		next(0);
+	});
 };
 
 module.exports = Execution;
