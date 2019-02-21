861a9290946a61cdbcaeb82ec25dfd414a7db955
remi <remi@remialkemade.nl>
2019-02-21 21:21:07 +0800

    Added waitForAll tests.

--- tests/test-execution.js
+++ tests/test-execution.js
@@ -4,10 +4,10 @@ const TestUtils = require('./qunit-test-utils');
 QUnit.module("Execution");
 
 QUnit.test("Utils.synchronize executes several functions in serial and maps the results.", function(assert) {
-	var done = TestUtils.async(assert, 1000);
+	let done = TestUtils.async(assert, 1000);
 
-	var order = '';
-	var steps = {
+	let order = '';
+	let steps = {
 		'foo': function() {
 			return new Promise((resolve, reject) => {
 				setTimeout(function(){
@@ -36,4 +36,83 @@ QUnit.test("Utils.synchronize executes several functions in serial and maps the 
 			assert.ok(false, "Synchronize succeeded.");
 			done();
 		});
+});
+
+QUnit.test("Utils.waitForAll waits for all promises to resolve.", function(assert) {
+  let done = TestUtils.async(assert, 1000);
+
+  let def1 = new Promise((resolve, reject) => resolve('a'));
+  let def2 = new Promise((resolve, reject) => resolve('b'));
+  let def3 = new Promise((resolve, reject) => resolve('c'));
+
+  let wait = Utils.waitForAll({
+    'd1': def1,
+    'd2': def2,
+    'd3': def3
+  });
+
+  wait.then(function(result) {
+    assert.deepEqual(result, {
+      'd1': 'a',
+      'd2': 'b',
+      'd3': 'c'
+    });
+    done();
+  });
+});
+
+QUnit.test("Utils.waitForAll rejects with error if one of the deferreds fails.", function(assert) {
+  let done = TestUtils.async(assert, 1000);
+  let def1 = new Promise((resolve, reject) => resolve('a'));
+  let def2 = new Promise((resolve, reject) => reject('b'));
+  let def3 = new Promise((resolve, reject) => resolve('c'));
+
+  let wait = Utils.waitForAll({
+    'd1': def1,
+    'd2': def2,
+    'd3': def3
+  });
+
+  wait.then(function(result) {
+    TestUtils.error(assert, "Should not resolve", result);
+    done();
+  });
+  wait.catch(function(err) {
+    assert.ok(err instanceof Error, "Failure passes instance of Error.");
+    assert.deepEqual(err.errorMap, {
+      'd2': 'b'
+    }, "Error map contains failing deferred.");
+    assert.deepEqual(err.data, {
+      'd1': 'a',
+      'd3': 'c'
+    }, "Error data contains successful data");
+    done();
+  });
+});
+
+QUnit.test("Utils.waitForAll rejects with error if timeout is passed.", function(assert) {
+  let done = TestUtils.async(assert, 1000);
+  let def1 = new Promise((resolve, reject) => resolve('a'));
+  let def2 = new Promise((resolve, reject) => reject('b'));
+  let def3 = new Promise((resolve, reject) => {});
+
+  let wait = Utils.waitForAll({
+    'd1': def1,
+    'd2': def2,
+    'd3': def3
+  }, 100);
+
+  wait.then(function(result) {
+    TestUtils.error(assert, "Should not resolve", result);
+    done();
+  });
+  wait.catch(function(err) {
+    assert.ok(err instanceof Error);
+    assert.deepEqual(err.data, {
+      'd1': 'a'
+    });
+    assert.ok('d2' in err.errorMap, 'd2 in errorMap');
+    assert.ok('d3' in err.errorMap, 'd3 in errorMap');
+    done();
+  });
 });
