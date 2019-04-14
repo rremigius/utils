56c13c7b524c0d745fb638438af08b1869bbd5f4
Remi <remialkemade@gmail.com>
2019-04-14 14:44:25 +0200

    Update src/event-interface.d.ts
    Added `on` and `fire` methods
    
    Co-Authored-By: spassvogel <exo_00@hotmail.com>

--- src/event-interface.d.ts
+++ src/event-interface.d.ts
@@ -3,4 +3,6 @@ export default class EventInterface {
 	extend():void;
 	getOnMethod():Function;
 	getFireMethod():Function;
+	on(event:string, callback:Function):void;
+	fire(event:string, data:object):void;
 }
