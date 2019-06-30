500d0de40b3a1fb5007dd641f5b7ea4fea4b8a43
remi <remi@remialkemade.nl>
2019-07-01 01:39:32 +0200

    Fixed EventInterface typings

--- src/event-interface.d.ts
+++ src/event-interface.d.ts
@@ -4,7 +4,7 @@ export default class EventInterface {
 	extend():void;
 	getOnMethod():(event:string, callback:Callback)=>void;
 	getOffMethod():(event:string, callback:Callback)=>void;
-	getFireMethod():(event:string, data:any)=>void;
+	getFireMethod():(event:string, data?:any)=>void;
 	on(event:string, callback:Function):void;
 	off(event:string, callback:Function):void;
 	fire(event:string, data:object):void;
@@ -14,5 +14,5 @@ type Callback = (data:any)=>void;
 export interface EventInterfacer {
 	on(event:string, callback:Callback):void;
 	off(event:string, callback:Callback):void;
-	fire(event:string, data:any):void;
+	fire(event:string, data?:any):void;
 }
